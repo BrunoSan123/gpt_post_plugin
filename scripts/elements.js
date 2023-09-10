0c6ebcbd86ddb340e8011810a3026840352f7d4a
Buno Barreto <Buno-kk@hotmail.com>
2023-09-10 02:01:19 -0300

    fix: gpt__model switch  in 70%

--- scripts/elements.js
+++ scripts/elements.js
@@ -1,21 +1,36 @@
 const palletSelect=document.querySelectorAll('.input_gpt')
 const pageBackground = document.getElementById("wpbody-content");
 const sidebar= document.getElementById("sidebar")
+const gpt_switch= document.querySelectorAll(".gpt_label_model")
+const switch_container=document.getElementById("chatgpt_model")
+const gpt_icons =document.querySelectorAll(".gpt_dash_icos")
 
 palletSelect.forEach((e,i)=>{
     e.addEventListener('change',()=>{
         if(e.getAttribute("name")=="DARK"){
             pageBackground.classList.add("dark")
             sidebar.classList.add("sidebar-dark")
+            switch_container.classList.add("sidebar-dark")
         }else{
             pageBackground.classList.remove("dark")
             sidebar.classList.remove("sidebar-dark")
+            switch_container.classList.remove("sidebar-dark")
         }
-        palletSelect.forEach((j)=>{
+        palletSelect.forEach((j,i)=>{
             if(j!==e){
                 j.checked=false;
             }
+
         })
 
+    })
+
+})
+
+gpt_switch.forEach((e,i)=>{
+    e.addEventListener('change',(i)=>{
+       if(i.target.getAttribute("data-target")=="ray"){
+        gpt_icons[0].childNodes[0].style="fill:blue !important"
+       }
     })
 })
--- templates/gpt_mode_swicth.php
+++ templates/gpt_mode_swicth.php
@@ -1,8 +1,9 @@
     <div id="chatgpt_model">
-        <?php $count=0;?>
+        <?php $count=0; $icons_modifier=array('ray','stars')?>
         <?php foreach ($models as $model_key => $model_name) : ?>
             <input type="radio" name="chatgpt_model" id="model_item-<?php echo $count?>" value="<?php echo esc_html($model_name)?>" class="gpt_input">
-            <label for="model_item-<?php echo $count?>">
+            <label for="model_item-<?php echo $count?>" class="gpt_label_model" data-target="<?php echo $icons_modifier[$count]?>">
+              <?php echo $icons[$count]?>
               <p><?php echo $model_name?></p>
             </label>
         <?php 
