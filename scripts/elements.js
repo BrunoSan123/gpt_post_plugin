e90b78bf7d1cc13e23baf9798e384028b95c8ed8
Bruno Barreto <Bruno-kk@hotmail.com>
2023-09-14 05:50:32 -0300

    fix: integração com DALL em 80%

--- scripts/elements.js
+++ scripts/elements.js
@@ -1,3 +1,6 @@
+const page_auto_post =document.body.classList.contains("settings_page_chatgpt_plugin")
+
+if(page_auto_post){
 const palletSelect=document.querySelectorAll('.input_gpt')
 const authorsDrop =document.querySelectorAll(".author_input")
 const pageBackground = document.getElementById("wpbody-content");
@@ -17,81 +20,88 @@ const gpt_config =document.querySelector(".chat_gpt_vonfiguration")
 const schedule_container= document.getElementById("schedule_datetime_container")
 const shcedule_input = document.querySelectorAll(".schedule_input")
 
+
 api_config.addEventListener('click',()=>{
-  gpt_config.classList.toggle("show")
-})
+    gpt_config.classList.toggle("show")
+  })
+  
+  palletSelect.forEach((e,i)=>{
+      e.addEventListener('change',()=>{
+          if(e.getAttribute("name")=="DARK"){
+              pageBackground.classList.add("dark")
+              sidebar.classList.add("sidebar-dark")
+              switch_container.classList.add("sidebar-dark")
+              gpt_textareas.forEach((e)=>{
+                  e.classList.add("dark")
+              })
+              selection_items.forEach((e)=>{
+                  e.classList.add("arrow_white")
+              })
+          }else{
+              pageBackground.classList.remove("dark")
+              sidebar.classList.remove("sidebar-dark")
+              switch_container.classList.remove("sidebar-dark")
+              gpt_textareas.forEach((e)=>{
+                  e.classList.remove("dark")
+              })
+              selection_items.forEach((e)=>{
+                  e.classList.remove("arrow_white")
+              })
+          }
+          palletSelect.forEach((j,i)=>{
+              if(j!==e){
+                  j.checked=false;
+              }
+  
+          })
+  
+      })
+  
+  })
+  
+  gpt_switch.forEach((e,i)=>{
+      e.addEventListener('change',(i)=>{
+         if(i.target.getAttribute("data-target")=="ray"){
+          gpt_icons[0].childNodes[0].style="fill:blue !important"
+         }
+      })
+  })
+  
+  authors_select.addEventListener('click',()=>{
+     authors_item.classList.toggle("show")
+  })
+  
+  categoty_select.addEventListener('click',()=>{
+    category_items.classList.toggle("show")
+  })
+  
+  authorsDrop.forEach((e,i)=>{
+      e.addEventListener('change',()=>{
+          authorsDrop.forEach((j,i)=>{
+              if(j!==e){
+                  j.checked=false;
+              }
+          })
+      })
+  })
+  
+  
+  shcedule_input.forEach((e)=>{
+      e.addEventListener('click',()=>{
+  
+          if(schedule_radio_button.checked){
+              schedule_container.classList.add("show")
+          }else{
+              schedule_container.classList.remove("show")
+          }
+      })
+  })
+  
 
-palletSelect.forEach((e,i)=>{
-    e.addEventListener('change',()=>{
-        if(e.getAttribute("name")=="DARK"){
-            pageBackground.classList.add("dark")
-            sidebar.classList.add("sidebar-dark")
-            switch_container.classList.add("sidebar-dark")
-            gpt_textareas.forEach((e)=>{
-                e.classList.add("dark")
-            })
-            selection_items.forEach((e)=>{
-                e.classList.add("arrow_white")
-            })
-        }else{
-            pageBackground.classList.remove("dark")
-            sidebar.classList.remove("sidebar-dark")
-            switch_container.classList.remove("sidebar-dark")
-            gpt_textareas.forEach((e)=>{
-                e.classList.remove("dark")
-            })
-            selection_items.forEach((e)=>{
-                e.classList.remove("arrow_white")
-            })
-        }
-        palletSelect.forEach((j,i)=>{
-            if(j!==e){
-                j.checked=false;
-            }
-
-        })
-
-    })
-
-})
-
-gpt_switch.forEach((e,i)=>{
-    e.addEventListener('change',(i)=>{
-       if(i.target.getAttribute("data-target")=="ray"){
-        gpt_icons[0].childNodes[0].style="fill:blue !important"
-       }
-    })
-})
-
-authors_select.addEventListener('click',()=>{
-   authors_item.classList.toggle("show")
-})
-
-categoty_select.addEventListener('click',()=>{
-  category_items.classList.toggle("show")
-})
-
-authorsDrop.forEach((e,i)=>{
-    e.addEventListener('change',()=>{
-        authorsDrop.forEach((j,i)=>{
-            if(j!==e){
-                j.checked=false;
-            }
-        })
-    })
-})
+}
 
 
-shcedule_input.forEach((e)=>{
-    e.addEventListener('click',()=>{
 
-        if(schedule_radio_button.checked){
-            schedule_container.classList.add("show")
-        }else{
-            schedule_container.classList.remove("show")
-        }
-    })
-})
 
 
 
